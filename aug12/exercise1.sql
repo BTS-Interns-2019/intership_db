b6d198690f9d2013ee27af18d0a8fed0902a65bc
rabbin <rabbin_cbta@hotmail.com>
2019-08-12 17:16:43 -0500

    insert data to database

--- aug12/exercise1.sql
+++ aug12/exercise1.sql
@@ -3,3 +3,4 @@ CREATE DATABASE bts_internship;
 USE bts_intership;
 CREATE TABLE students ( first_name VARCHAR(120), last_name VARCHAR(120), grade_1 TINYINT(2), grade_2 TINYINT(2), grade_3 TINYINT(2), average DECIMAL(2,2));
 describe students;
+INSERT INTO students VALUES ("Juan", "Perez", 8, 8, 9, 0),("Apu", "Nahasapimapeti", 7, 8 ,9, 0), ("Emmanuel", "Villalobos Arronte", 7, 7, 7, 0), ("Pepe", NULL, 8, 5, 6, 0), ("Juan", "Perez", 6, 4, 5, 0);
