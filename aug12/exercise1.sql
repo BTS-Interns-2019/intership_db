32a56a2e660282954089827bdaa3129d6e3483f9
Urbano <urbano.gonzalez@alumnos.udg.mx>
2019-08-12 18:51:37 -0500

    DB Exercise1

--- aug12/exercise1.sql
+++ aug12/exercise1.sql
@@ -8,3 +8,15 @@ CREATE TABLE students (
     grade_3 TINYINT(2) UNSIGNED,
     average DECIMAL(2,2)
 );
+
+INSERT INTO students (first_name, last_name, grade_1, grade_2, grade_3, average) VALUES
+    ('Juan','Perez',8,8,9,0),
+    ('Apu','Nahasapimapeti',7,8,9,0),
+    ('Emmanuel','Villalobos Arronte',7,7,7,0),
+    ('Pepe',NULL,8,5,6,0),
+    ('Juan','Perez',8,8,9,0);
+     /**
+     Borrar columna
+     **/
+ALTER TABLE students DROP COLUMN average;
+ALTER TABLE students ADD COLUMN id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY FIRST;
