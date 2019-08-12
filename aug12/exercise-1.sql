909e199d6fcbc7866c15d9ebf4c08a85b842dc2f
unknown <luiscarlosg.bts@gmail.com>
2019-08-12 16:38:52 -0500

    exercise 1

---
+++ aug12/exercise-1.sql
@@ -0,0 +1,4 @@
+create database bts_intership;
+USE bts_intership;
+create table students(first_name VARCHAR(100), last_name VARCHAR(100),grade_1 TINYINT(2), grade_2 TINYINT(2), grade_3 TINYINT(2),average DECIMAL(2,2));
+
