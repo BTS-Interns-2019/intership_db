f7bcaa5b2c27fad0c47fed0d746a744490171744
Hector Soto <hectors.bts@gmail.com>
2019-08-12 18:47:20 -0500

    Add id to table

--- aug12/exercise1.sql
+++ aug12/exercise1.sql
@@ -1,6 +1,8 @@
 CREATE DATABASE bts_internship; -- Create database
 USE bts_internship; -- Move to the newly created database
-CREATE TABLE students ( -- Create a new students table
+-- Create a new students table
+CREATE TABLE students (
+  id INT UNSIGNED PRIMARY 
   first_name VARCHAR(120),
   last_name VARCHAR(120),
   grade_1 TINYINT(2) UNSIGNED,
@@ -8,8 +10,17 @@ CREATE TABLE students ( -- Create a new students table
   grade_3 TINYINT(2) UNSIGNED,
   average DECIMAL(2,2),
 );
+
+-- Insert values into table
 INSERT INTO students (first_name, last_name, grade_1, grade_2, grade_3, average) VALUES ('Juan', 'Perez', 8, 8, 9, 0);
 INSERT INTO students (first_name, last_name, grade_1, grade_2, grade_3, average) VALUES ('Apu', 'Nahasapimapeti', 7, 8, 9, 0);
 INSERT INTO students (first_name, last_name, grade_1, grade_2, grade_3, average) VALUES ('Emmanuel', 'Villalobos Arronte', 7, 7, 7, 0);
 INSERT INTO students (first_name, last_name, grade_1, grade_2, grade_3, average) VALUES ('pepe', NULL, 8, 5, 6, 0);
 INSERT INTO students (first_name, last_name, grade_1, grade_2, grade_3, average) VALUES ('Juan', 'Perez', 6, 4, 5, 0);
+
+-- Get average between the grades for each row
+SELECT *, (grade_1 + grade_2 + grade_3)/3 AS AVERAGE FROM students;
+-- Drop average column
+ALTER TABLE students DROP COLUMN average;
+-- Add primary key column to table
+ALTER TABLE students ADD COLUMN id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY FIRST;
