8df368000edbe73a98b79f825088ad04892bf047
unknown <luiscarlosg.bts@gmail.com>
2019-08-12 17:09:19 -0500

    insert

--- aug12/exercise-1.sql
+++ aug12/exercise-1.sql
@@ -4,7 +4,7 @@ create table students(first_name VARCHAR(100), last_name VARCHAR(100),grade_1 TI
 insert into students values ('Juan','Perez',8,8,9,0);
 insert into students values ('Apu','Nahasapimapeti',7,8,9,0);
 insert into students values ('Emmanuel','Villalobos Arronte',7,7,7,0);
-insert into students values ('pepe','',8,5,6,0);
+insert into students values ('pepe',null,8,5,6,0);
 insert into students values ('Juan','Perez',6,4,5,0);
 
 
