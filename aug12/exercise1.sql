81bded494db8ebb89dd9970edc9b47ffe3033e30
estefaniacr <estefaniac.bts@gmail.com>
2019-08-12 18:53:36 -0500

    exercise1 version final

--- aug12/exercise1.sql
+++ aug12/exercise1.sql
@@ -1,6 +1,7 @@
 create database bts_internship;
 show databases;
 use bts_internship;
+CREATE TABLE students (first_name VARCHAR(50), last_name VARCHAR(50), grade_1 TINYINT(2) unsigned, grade_2 TINYINT(2) unsigned, grade_3 TINYINT(2) unsigned, average DECIMAL(5,3) unsigned);
 show tables;
 describe students;
 INSERT INTO students VALUES('Juan', 'Perez', 8, 8, 9, 0);
@@ -8,7 +9,6 @@ INSERT INTO students VALUES('Apu', 'Nahasapimapeti', 7, 8 , 9, 0);
 INSERT INTO students VALUES('Emmanuel', 'Villalobos Arronte', 7, 7, 7, 0);
 INSERT INTO students VALUES('pepe', '', 8, 5, 6, 0);
 INSERT INTO students VALUES('Juan', 'Perez', 6, 4, 5, 0);
-show table status;
 SELECT * FROM students;
 SELECT * FROM students WHERE first_name = 'Juan';
 SELECT * FROM students WHERE first_name = 'juan';
