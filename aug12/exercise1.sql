48a96c23ac8275d3a4809ed56f204d87482fe746
Marlon Torres <marlont.bts@gmail.com>
2019-08-12 18:51:48 -0500

    exercise1 terminado completamente

--- aug12/exercise1.sql
+++ aug12/exercise1.sql
@@ -8,3 +8,15 @@ CREATE TABLE students (
     grade_3 TINYINT (2) UNSIGNED,
     average DECIMAL (2,2) UNSIGNED
 );
+
+INSERT INTO students (first_name, last_name, grade_1, grade_2, grade_3, average) VALUES ('Juan', 'Perez', 8, 8, 9, 0);
+INSERT INTO students (first_name, last_name, grade_1, grade_2, grade_3, average) VALUES ('Apu', 'Nahasapimapeti', 7, 8, 9, 0);
+INSERT INTO students (first_name, last_name, grade_1, grade_2, grade_3, average) VALUES ('Emmanuel', 'Villalobos Aronte', 7, 7, 7, 0);
+INSERT INTO students (first_name, grade_1, grade_2, grade_3, average) VALUES ('pepe', 8, 5, 6, 0);
+INSERT INTO students (first_name, last_name, grade_1, grade_2, grade_3, average) VALUES ('Juan', 'Perez', 6, 4, 5, 0);
+
+SELECT first_name, last_name, grade_1, grade_2, grade_3, ((grade_1 + grade_2 + grade_3) / 3) 'average' FROM students;
+
+ALTER TABLE students DROP average;
+
+ALTER TABLE students ADD id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT FIRST;
