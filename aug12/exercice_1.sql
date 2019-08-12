f644a1c1c10cb75a38c02b3cb3393d7146345b1f
unknown <cristianliljon@hotmail.com>
2019-08-12 17:11:48 -0500

    Insert in table students done

--- aug12/exercice_1.sql
+++ aug12/exercice_1.sql
@@ -7,3 +7,11 @@ CREATE TABLE students (first_name VARCHAR(120), last_name VARCHAR(120),
 grade_1 TINYINT(2), grade_2 TINYINT(2), grade_3 TINYINT(2),
 average DECIMAL(2,2));
 
+INSERT INTO students VALUES("Juan","Perez",8,8,9,0),
+("Apu","Nahasapimapeti",7,8,9,0);
+
+INSERT INTO students VALUES
+("Emmanuel", "Villalobos Arronte",7,7,7,0),
+("pepe","",8,5,6,0),("Juan","Perez",6,4,5,0);
+
+UPDATE students SET last_name = null WHERE first_name = "pepe";
