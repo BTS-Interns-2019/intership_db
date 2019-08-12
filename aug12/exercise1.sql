40d528f65b4bf4193da39401248f11bd257b1100
Urbano <urbano.gonzalez@alumnos.udg.mx>
2019-08-12 16:55:49 -0500

    Exercise 1 finished

---
+++ aug12/exercise1.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE bts_internship;
+USE bts_internship;
+CREATE TABLE students (
+    first_name VARCHAR(120),
+    last_name VARCHAR(120),
+    grade_1 TINYINT(2) UNSIGNED,
+    grade_2 TINYINT(2) UNSIGNED,
+    grade_3 TINYINT(2) UNSIGNED,
+    average DECIMAL(2,2)
+);
