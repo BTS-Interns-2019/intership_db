fd5d804c4dbd02aedec14320dc2fb642d0de8ee0
silva-david <silvad.bts@gmail.com>
2019-08-12 19:05:21 -0500

    Primer versión exercise1 - David

---
+++ aug12/exercise1.sql
@@ -0,0 +1,20 @@
+mysql -p -u root
+CREATE DATABASE bts_internship;
+SHOW DATABASES;
+USE bts_internship;
+CREATE TABLE students (first_name VARCHAR(30), last_name VARCHAR(30), grade_1 TINYINT(2) unsigned, grade_2 TINYINT(2) unsigned, grade_3 TINYINT(2) unsigned, average DECIMAL(5,2));
+SHOW TABLES;
+DESCRIBE students;
+INSERT INTO students VALUES ('Juan', 'Perez', 8, 8, 9, 0);
+INSERT INTO students VALUES ('Apu', 'Nahasapeemapetilon', 7, 8, 9, 0);
+INSERT INTO students VALUES ('Emmanuel', 'Villalobos Arronte', 7, 7, 7, 0);
+INSERT INTO students VALUES ('pepe', '', 8, 5, 6, 0);
+INSERT INTO students VALUES ('Juan', 'Perez', 6, 4, 5, 0);
+SELECT * FROM students;
+SELECT * FROM students WHERE first_name = 'Juan';
+SELECT * FROM students WHERE first_name = 'juan';
+SELECT * FROM students WHERE UCASE(first_name) = UCASE('juan');
+SELECT average FROM students;
+SELECT UCASE(first_name) FROM students;
+SELECT (grade_1 + grade_2 + grade_3)/3 FROM students;
+SELECT *, (grade_1 + grade_2 + grade_3)/3 AS promedio FROM students;
