beca6db25b9d5a61a50340a86f88d2b6f2c3fea7
Marlon Torres <marlont.bts@gmail.com>
2019-08-13 15:17:27 -0500

    pequeño cambio

--- studentsTable.sql
+++ studentsTable.sql
@@ -4,6 +4,7 @@ LIMIT 0, 1000
 
 -- Date: 2019-08-12 18:43
 */
+USE `bts_internship`;
 INSERT INTO `students` (`first_name`,`last_name`,`grade_1`,`grade_2`,`grade_3`) VALUES ('Juan','Perez',8,8,9);
 INSERT INTO `students` (`first_name`,`last_name`,`grade_1`,`grade_2`,`grade_3`) VALUES ('Apu','Nahasapimapeti',7,8,9);
 INSERT INTO `students` (`first_name`,`last_name`,`grade_1`,`grade_2`,`grade_3`) VALUES ('Emmanuel','Villalobos Arronte',7,7,7);
