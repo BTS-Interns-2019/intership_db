6e955b302a230f045f8e5156a7539e9f55af4a2f
emmanuelr.bts@gmail.com <emmanuel.lv.97@gmail.com>
2019-08-13 16:26:24 -0500

    ejercicios 2 y 3; normalización de la base de datos y script de migración

---
+++ aug13/exercise3.sql
@@ -0,0 +1,11 @@
+CREATE TABLE grades(
+    id_student INTEGER UNSIGNED,
+    week TINYINT UNSIGNED,
+    grade TINYINT(2) UNSIGNED
+);
+
+INSERT INTO grades (id_student, week, grade) SELECT id, 1, grade_1 FROM students;
+INSERT INTO grades (id_student, week, grade) SELECT id, 2, grade_2 FROM students;
+INSERT INTO grades (id_student, week, grade) SELECT id, 3, grade_3 FROM students;
+
+ALTER TABLE students DROP COLUMN grade_1, DROP COLUMN grade_2, DROP COLUMN grade_3;
