c68b1fdf7d7ffa685f2e26580a71004cde84cf27
silva-david <silvad.bts@gmail.com>
2019-08-20 15:01:14 -0500

    Primer version exercise - David

---
+++ aug19/exercise.sql
@@ -0,0 +1,13 @@
+SELECT country.country, category.name, COUNT(*) AS Total
+FROM category
+JOIN film_category ON category.category_id = film_category.category_id
+JOIN film ON film_category.film_id = film.film_id
+JOIN inventory ON film.film_id = inventory.film_id
+JOIN rental ON inventory.inventory_id = rental.inventory_id
+JOIN customer ON rental.customer_id = customer.customer_id
+JOIN address ON customer.address_id = address.address_id
+JOIN city ON address.city_id = city.city_id
+JOIN country ON city.country_id = country.country_id
+GROUP BY country.country, category.name
+ORDER BY country.country 
+;
