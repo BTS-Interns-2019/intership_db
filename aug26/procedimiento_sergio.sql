8bf0a31ef1f14b404fa53fef45db03f5d3e1b449
Sergio Rivera <telesforociriaco@gmail.com>
2019-08-26 23:55:02 -0500

    intento de procedimiento sergio

---
+++ aug26/procedimiento_sergio.sql
@@ -0,0 +1,34 @@
+USE `sakila`;
+DROP procedure IF EXISTS `rented`;
+
+DELIMITER $$
+USE `sakila`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `rented`(inventory_id INT, customer_id INT, staff_id INT)
+BEGIN
+DECLARE id INT;
+DECLARE count INT;
+	SET autocommit = 0;
+    START transaction;
+    savepoint pointer;
+    INSERT INTO rental(rental_date, inventory_id, customer_id,return_date, staff_id, last_update)
+    VALUES(now(), inventory_id, customer_id,DATE_ADD(now(), interval 15 DAY), staff_id, now());
+    
+    SELECT LAST_INSERT_ID() INTO @id FROM rental;
+    
+    INSERT INTO payment(customer_id, staff_id, rental_id, amount, payment_date, last_update)
+    VALUES(customer_id, staff_id, @id, 6.66, now(), now());
+    
+   IF (SELECT return_date FROM rental WHERE rental.inventory_id = inventory_id) IS NULL
+		THEN SELECT 'esta rentada';
+        rollback to pointer;
+    ELSE
+		COMMIT;
+		SELECT 'available';
+    END IF;
+    
+    SET autocommit = 1;
+END$$
+
+DELIMITER ;
+
+
